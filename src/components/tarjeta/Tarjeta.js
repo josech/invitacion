a11f4ee8677808746e5863c3e4553ae529306c91
JoseCh <xjosechx@gmail.com>
2021-12-29 23:38:08 -0500

    Tarjeta responsive

--- src/components/tarjeta/Tarjeta.js
+++ src/components/tarjeta/Tarjeta.js
@@ -4,10 +4,10 @@ import "./Tarjeta.css";
 function Tarjeta(){
     return (
         <div className="tarjeta">
-            <div>
-                <span>De común acuerdo y en plena posesión de nuestras facultades </span>
-                <p>mentales (sean las que sean) hemos decidido</p>
-                <p>¡Casarnos!</p>
+            <div className="tarjeta-content">
+                De común acuerdo y en plena posesión de nuestras facultades <br />
+                mentales (sean las que sean) hemos decidido <br />
+                ¡Casarnos!
             </div>
         </div>
     )
