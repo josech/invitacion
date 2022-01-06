f37e15d4d2bcd9963eb2e9b0980fefaccd5c57f0
JoseCh <xjosechx@gmail.com>
2022-01-06 17:34:55 -0500

    clean page

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 import './Page.css';
-import top_l from '../assets/images/top-l.png';
-import top_r from '../assets/images/top-r.png';
-import bottom_l from '../assets/images/bottom-l.png';
-import bottom_r from '../assets/images/bottom-r.png';
 import {Home} from './Home.js';
 import {Invitacion} from './Invitacion.js';
 
@@ -20,18 +16,6 @@ function Page() {
     
     return (
         <div className="app">
-            {/* <div className="app__top app__top--r">
-                <img src={`${top_r}`} alt=""></img>
-            </div>
-            <div className="app__top app__top--l">
-                <img src={`${top_l}`} alt=""></img>
-            </div>
-            <div className="app__bottom app__bottom--r">
-                <img src={`${bottom_r}`} alt=""></img>
-            </div>
-            <div className="app__bottom app__bottom--l">
-                <img src={`${bottom_l}`} alt=""></img>
-            </div> */}
             <Home 
                 persona={persona}
                 setPersona={setPersona}
